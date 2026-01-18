5777f8b2e3c39564ece5d17c1d58e30c063cdb60
Nahuel404 <nahuelmarincivil@gmail.com>
2026-01-18 18:46:48 -0300

    Gym-app beta 0.0.1

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  site: "https://gymapp.not404.dev",
+  output: "server",
   adapter: netlify(),
   vite: {
     ssr: {
-      external: ['crypto', 'node:crypto']
-    }
-  }
+      external: ["crypto", "node:crypto"],
+    },
+  },
 });
